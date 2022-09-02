88f6d02742a2cd827716ac94c6bc5be415083ddc
MK-Khan123 <mehnazkhan231@gmail.com>
2022-09-02 20:37:16 +0600

    3. Creating & Using a Store

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-container title="Vuex">
+    <h3>{{ $store.state.counter }}</h3>
+    <button>Add 1</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+  data() {},
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
