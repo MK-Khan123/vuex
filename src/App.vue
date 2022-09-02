bc5b6fbdec2148e5a3c99edacb850de9a4e27b2a
MK-Khan123 <mehnazkhan231@gmail.com>
2022-09-02 21:23:29 +0600

    4. Connecting Components to State

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <base-container title="Vuex">
-    <h3>{{ $store.state.counter }}</h3>
-    <button>Add 1</button>
+    <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheContainer.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
   },
-  data() {},
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    },
+  },
 };
 </script>
 
